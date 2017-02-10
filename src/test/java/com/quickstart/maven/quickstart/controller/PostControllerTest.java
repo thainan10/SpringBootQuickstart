12b4d6f17aff8c8fd4d549fe1dfa345cc9aad818
Thainan Matias <thainan10@gmail.com>
2017-02-10 14:18:11 -0300

    Implements PostControllerTest

--- src/test/java/com/quickstart/maven/quickstart/controller/PostControllerTest.java
+++ src/test/java/com/quickstart/maven/quickstart/controller/PostControllerTest.java
@@ -1,5 +1,28 @@
 package com.quickstart.maven.quickstart.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PostController.class)
 public class PostControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void shouldReturnPost() throws Exception {
+		this.mockMvc.perform(get("/posts/")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 
 }
--- src/test/java/com/quickstart/maven/quickstart/dao/PostDaoTest.java
+++ src/test/java/com/quickstart/maven/quickstart/dao/PostDaoTest.java
@@ -5,14 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.quickstart.maven.quickstart.App;
 import com.quickstart.maven.quickstart.model.Post;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostDaoTest {
 	@Autowired
 	private PostDao postDao;
